9ba51fcd061b7c023409889eadf00b33416c591b
manojgowda <19eren.yeager.96@gmail.com>
2024-04-16 17:40:04 +0530

    added minor changes

--- animebud/wsgi.py
+++ animebud/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'animebud.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "animebud.settings")
 
 application = get_wsgi_application()
--- base/forms.py
+++ base/forms.py
@@ -5,6 +5,6 @@ from .models import Room
 class RoomForm(ModelForm):
     class Meta:
         model = Room
-        fields = '__all__'
-        exclude =['host','participants']
+        fields = "__all__"
+        exclude =["host','participants"]
                   
